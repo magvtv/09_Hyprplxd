5c301034f6c0778710ab5bcf11bdece5e5eb2f6e
magvtv <magutujr@gmail.com>
2023-05-30 17:42:14 +0300

    added new branch: leftnav, commented the onclick functions

--- Web Grid/webgrid.js
+++ Web Grid/webgrid.js
@@ -2,18 +2,28 @@ let activeIndex = 0;
 const articles = document.getElementsByTagName("article")
 const totalArticles = articles.length - 1;
 const handleRightClick = () => {
+    // increase the next index by 1, moving the article rightwards
     const nextIndex = activeIndex + 1 <= (totalArticles) ? activeIndex + 1 : 0;
     const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`), nextArticle = document.querySelector(`[data-index="${nextIndex}"]`);
     
+
+    // active article becomes new 'after'
     currentArticle.dataset.status = "after" 
+
+    // next article comes on-screen (active)
     nextArticle.dataset.status = "on";
     activeIndex = nextIndex;
 }
 
 const handleLeftClick = () => {
+    // decrease the current index by 1, moving the article leftwards
     const prevIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : totalArticles - 1;
     const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`), prevArticle = document.querySelector(`[data-index="${prevIndex}"]`);
+
+    // active article becomes new 'after'
     currentArticle.dataset.status = "before";
+
+    // previous article comes on-screen (active)
     prevArticle.dataset.status = "on";
     activeIndex = prevIndex;
 }
