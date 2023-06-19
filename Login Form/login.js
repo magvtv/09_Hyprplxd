2138d6f57d76a1ee7a79ef53802d2dada44feed0
magvtv <magutujr@gmail.com>
2023-06-19 06:12:04 +0300

    commented out the map code

--- Login Form/login.js
+++ Login Form/login.js
@@ -1,14 +1,21 @@
 
-const sounds = new Map()
-sounds.set("dog", "woof")
-sounds.set("cat", "meow")
-sounds.set("duck", "quack")
-sounds.set("turkey", "gobbles")
-sounds.set("lion", "roar")
+// const sounds = new Map()
+// sounds.set("dog", "woof")
+// sounds.set("cat", "meow")
+// sounds.set("duck", "quack")
+// sounds.set("turkey", "gobbles")
+// sounds.set("lion", "roar")
 
-for (const [key, value] of sounds) {
-    console.log(`${key} goes ${value}`)
-} 
+// for (const [key, value] of sounds) {
+//     console.log(`${key} goes ${value}`)
+// } 
 
 
-constructor
+function bitch (citizen, age, bodycount) {
+    this.citizen = citizen
+    this.age = age
+    this.bodycount = bodycount
+}
+
+let Vaxalia = new bitch("South Korean", 20, 0)
+console.log(Vaxalia.citizen)
