5c9ac3b4b2b1ac89f52124f2382ce7bee052ee25
magvtv <magutujr@gmail.com>
2023-06-20 17:25:50 +0300

    renamed Login Form folder to WeAreJS

---
+++ WeAreJS/login.js
@@ -0,0 +1,45 @@
+
+// const sounds = new Map()
+// sounds.set("dog", "woof")
+// sounds.set("cat", "meow")
+// sounds.set("duck", "quack")
+// sounds.set("turkey", "gobbles")
+// sounds.set("lion", "roar")
+
+// for (const [key, value] of sounds) {
+//     console.log(`${key} goes ${value}`)
+// } 
+
+
+// function bitch (citizen, age, bodycount) {
+//     this.citizen = citizen
+//     this.age = age
+//     this.bodycount = bodycount
+// }
+
+// let Vaxalia = new bitch("South Korean", 20, 0)
+// console.log(Vaxalia.citizen)
+
+// const body = document.body
+// body.querySelector(".button").addEventListener("click", () => {
+//     console.log("You pressed the button!")
+// })
+
+// function doStep1(x) {
+//     return(`${x}`)
+// }
+
+// function doStep2(x) {
+//     return (`${x} we`)
+// }
+
+
+// function doStep3(x) {
+//     return (`${x} can`)
+// }
+
+// let word = "Yes";
+// word = doStep1(word)
+// word = doStep2(word)
+// word = doStep3(word)
+// console.log(word)
