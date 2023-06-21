ae297434775916b21a80f76391807c5e9408d8b5
magvtv <magutujr@gmail.com>
2023-06-21 18:26:09 +0300

    commented out the code, starting on promises

--- WeAreJS/login.js
+++ WeAreJS/login.js
@@ -44,28 +44,28 @@
 // word = doStep3(word)
 // console.log(word)
 
-const firstWord = (initial, callback) => {
-    const word = `${initial}, PH`;
-    callback(word);
-}
-const secondWord = (initial, callback) => {
-    const word = `${initial}. Welcome`;
-    callback(word);
-}
+// const firstWord = (initial, callback) => {
+//     const word = `${initial}, PH`;
+//     callback(word);
+// }
+// const secondWord = (initial, callback) => {
+//     const word = `${initial}. Welcome`;
+//     callback(word);
+// }
 
-const thirdWord = (initial, callback) => {
-    const word = `${initial} Home.`;
-    callback(word);
-}
+// const thirdWord = (initial, callback) => {
+//     const word = `${initial} Home.`;
+//     callback(word);
+// }
 
-function doEverything() {
-    firstWord("Hello", word1 => {
-        secondWord(word1, word2 => {
-            thirdWord(word2, word3  => {
-                console.log(`${word3}`)
-            });
-        });
-    });
-}
+// function doEverything() {
+//     firstWord("Hello", word1 => {
+//         secondWord(word1, word2 => {
+//             thirdWord(word2, word3  => {
+//                 console.log(`${word3}`)
+//             });
+//         });
+//     });
+// }
 
 doEverything()
