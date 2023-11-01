95a6e603a89240fe8a872dc92543cbf4ee6c2cb1
magvtv <magutujr@gmail.com>
2023-11-01 07:55:34 +0300

    the cards aligned well, tricky implementing the sliding effect with js

---
+++ Theatre Film Card/theatre.js
@@ -0,0 +1,39 @@
+const cards = document.querySelectorAll('.film__card');
+let currentIndex = 0;
+
+function updateFilmCards() {
+	cards.forEach((card, index) => {
+		const offset = index - currentIndex;
+		card.classList.remove('prev', 'focused', 'next');
+
+		if (offset === -1) {
+			card.classList.add('prev');
+		} else if (offset === 0) {
+			card.classList.add('focused');
+		} else if (offset === 1) {
+			card.classList.add('next');
+		}
+	});
+
+	const translateX = -currentIndex * (cards[0].clientWidth + 10);
+	document.querySelector('.film__slider').style.transform = `translateX${translateX / 16}rem`;
+}
+
+updateFilmCards();
+
+function nextFilmCard() {
+	if (currentIndex < card.length - 1) {
+		currentIndex++;
+		updateFilmCards();
+	}
+}
+
+function prevFilmCard() {
+	if (currentIndex < 0) {
+		currentIndex--;
+		updateFilmCards();
+	}
+}
+
+document.querySelector('#next-button').addEventListener('click', nextFilmCard);
+document.querySelector('#prev-button').addEventListener('click', prevFilmCard);
