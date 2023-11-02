e4e1feaf1ace57905157c0e94ee40f1d14e75e32
magvtv <magutujr@gmail.com>
2023-11-02 16:09:21 +0300

    optimized the interactivity, still updating the js file

--- Carousels/DragWheel/drag.js
+++ Carousels/DragWheel/drag.js
@@ -1,80 +1,55 @@
-/*--------------------
-Vars
---------------------*/
 let progress = 50;
 let startX = 0;
-let active = 0;
 let isDown = false;
-
-/*--------------------
-Contants
---------------------*/
 const speedWheel = 0.02;
 const speedDrag = -0.1;
 
-/*--------------------
-Get Z
---------------------*/
-const getZindex = (array, index) =>
-	array.map((_, i) => (index === i ? array.length : array.length - Math.abs(index - i)));
-
-/*--------------------
-Items
---------------------*/
 const $items = document.querySelectorAll('.carousel-item');
 const $cursors = document.querySelectorAll('.cursor');
 
+const getZindex = (array, index) => array.map((_, i) => (index === i ? array.length : array.length - Math.abs(index - i));
 const displayItems = (item, index, active) => {
-	const zIndex = getZindex([...$items], active)[index];
-	item.style.setProperty('--z-index', zIndex);
-	item.style.setProperty('--active', (index - active) / $items.length);
+    const zIndex = getZindex([...$items], active)[index];
+    item.style.setProperty('--z-index', zIndex);
+    item.style.setProperty('--active', (index - active) / $items.length);
 };
-
 
 const animate = () => {
-	progress = Math.max(0, Math.min(progress, 100));
-	active = Math.floor((progress / 100) * ($items.length - 1));
-
-	$items.forEach((item, index) => displayItems(item, index, active));
+    progress = Math.max(0, Math.min(progress, 100));
+    const active = Math.floor((progress / 100) * ($items.length - 1));
+    $items.forEach((item, index) => displayItems(item, index, active));
 };
+
 animate();
 
-
-$items.forEach((item, i) => {
-	item.addEventListener('click', () => {
-		progress = (i / $items.length) * 100 + 10;
-		animate();
-	});
-});
-
+$items.forEach((item, i) => item.addEventListener('click', () => {
+    progress = (i / $items.length) * 100 + 10;
+    animate();
+}));
 
 const handleWheel = (e) => {
-	const wheelProgress = e.deltaY * speedWheel;
-	progress = progress + wheelProgress;
-	animate();
+    progress += e.deltaY * speedWheel;
+    animate();
 };
 
 const handleMouseMove = (e) => {
-	if (e.type === 'mousemove') {
-		$cursors.forEach(($cursor) => {
-			$cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-		});
-	}
-	if (!isDown) return;
-	const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
-	const mouseProgress = (x - startX) * speedDrag;
-	progress = progress + mouseProgress;
-	startX = x;
-	animate();
+    if (e.type === 'mousemove') {
+        $cursors.forEach($cursor => $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`);
+    }
+    if (!isDown) return;
+    const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+    progress += (x - startX) * speedDrag;
+    startX = x;
+    animate();
 };
 
 const handleMouseDown = (e) => {
-	isDown = true;
-	startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+    isDown = true;
+    startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
 };
 
 const handleMouseUp = () => {
-	isDown = false;
+    isDown = false;
 };
 
 document.addEventListener('mousewheel', handleWheel);
